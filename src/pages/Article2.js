d89ce17837976c16a6c16b7ed5179bff4452c865
Ivo Desktop <ivo.k.eilertsen@hiof.no>
2022-10-07 13:00:54 +0200

    qol update article 2

--- src/pages/Article2.js
+++ src/pages/Article2.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { ChartSRenteMnd } from "../components/ChartSRenteMnd";
 import { ChartSRenteÅr } from "../components/ChartSRenteÅr";
-/* import InfographDesktop from "../images/infographic1.png"; */
+import InfographDesktop from "../images/infographic1.png";
 import Erlend from "../images/erlend.png";
 import InfographMobile from "../images/infographMobile.png";
 
@@ -67,7 +67,8 @@ export default function Article2() {
           Folk har litt dårligere råd og handler dermed mindre, da blir prisene
           lavere og lønninger reduseres. Dermed blir inflasjon dempet.
         </p>
-        <img className={"infograph"} src={InfographMobile} alt="" />
+        <img className={"infographMobile"} src={InfographMobile} alt="" />
+        <img className={"infographDesktop"} src={InfographDesktop} alt="" />
         <br></br>
         <h2>Dette sier eksperten</h2>
         <figure>
