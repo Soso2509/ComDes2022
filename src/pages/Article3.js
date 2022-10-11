afeee583924b0a9fd6e6e023f62122f587c658c8
Sofie Helene Karlsen <sofiehk@gmail.com>
2022-10-11 13:20:37 +0200

    Lagt inn kart og begynnelse av styling

--- src/pages/Article3.js
+++ src/pages/Article3.js
@@ -1,4 +1,5 @@
 import React from "react";
+import MapChart from "../components/Map";
 
 export default function Article3() {
   return (
@@ -6,18 +7,62 @@ export default function Article3() {
       <section className={"plasticBottle"}></section>
       <h1 className={"article3Title"}>Klimagreier</h1>
       <span className={"articleUnderTitle"}>Lorem ipsum dolor sit amet</span>
-      <section className={"articleIntro"}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</section>
+      <section className={"articleIntro"}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua
+      </section>
       <section className={"articleContent"}>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse platea dictumst vestibulum rhoncus est. In massa tempor nec feugiat nisl pretium. Adipiscing enim eu turpis egestas pretium aenean pharetra magna ac. Scelerisque felis imperdiet proin fermentum. Duis at tellus at urna condimentum mattis. Nunc sed augue lacus viverra vitae congue eu consequat. Donec pretium vulputate sapien nec sagittis aliquam malesuada. Magna fermentum iaculis eu non diam phasellus. Condimentum mattis pellentesque id nibh tortor.</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse
+          platea dictumst vestibulum rhoncus est. In massa tempor nec feugiat
+          nisl pretium. Adipiscing enim eu turpis egestas pretium aenean
+          pharetra magna ac. Scelerisque felis imperdiet proin fermentum. Duis
+          at tellus at urna condimentum mattis. Nunc sed augue lacus viverra
+          vitae congue eu consequat. Donec pretium vulputate sapien nec sagittis
+          aliquam malesuada. Magna fermentum iaculis eu non diam phasellus.
+          Condimentum mattis pellentesque id nibh tortor.
+        </p>
         <br></br>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse platea dictumst vestibulum rhoncus est. In massa tempor nec feugiat nisl pretium. Adipiscing enim eu turpis egestas pretium aenean pharetra magna ac. Scelerisque felis imperdiet proin fermentum. Duis at tellus at urna condimentum mattis. Nunc sed augue lacus viverra vitae congue eu consequat. Donec pretium vulputate sapien nec sagittis aliquam malesuada. Magna fermentum iaculis eu non diam phasellus. Condimentum mattis pellentesque id nibh tortor.</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse
+          platea dictumst vestibulum rhoncus est. In massa tempor nec feugiat
+          nisl pretium. Adipiscing enim eu turpis egestas pretium aenean
+          pharetra magna ac. Scelerisque felis imperdiet proin fermentum. Duis
+          at tellus at urna condimentum mattis. Nunc sed augue lacus viverra
+          vitae congue eu consequat. Donec pretium vulputate sapien nec sagittis
+          aliquam malesuada. Magna fermentum iaculis eu non diam phasellus.
+          Condimentum mattis pellentesque id nibh tortor.
+        </p>
       </section>
       <p>kart</p>
       <section className={"articleContent"}>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse platea dictumst vestibulum rhoncus est. In massa tempor nec feugiat nisl pretium. Adipiscing enim eu turpis egestas pretium aenean pharetra magna ac. Scelerisque felis imperdiet proin fermentum. Duis at tellus at urna condimentum mattis. Nunc sed augue lacus viverra vitae congue eu consequat. Donec pretium vulputate sapien nec sagittis aliquam malesuada. Magna fermentum iaculis eu non diam phasellus. Condimentum mattis pellentesque id nibh tortor.</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse
+          platea dictumst vestibulum rhoncus est. In massa tempor nec feugiat
+          nisl pretium. Adipiscing enim eu turpis egestas pretium aenean
+          pharetra magna ac. Scelerisque felis imperdiet proin fermentum. Duis
+          at tellus at urna condimentum mattis. Nunc sed augue lacus viverra
+          vitae congue eu consequat. Donec pretium vulputate sapien nec sagittis
+          aliquam malesuada. Magna fermentum iaculis eu non diam phasellus.
+          Condimentum mattis pellentesque id nibh tortor.
+        </p>
         <br></br>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse platea dictumst vestibulum rhoncus est. In massa tempor nec feugiat nisl pretium. Adipiscing enim eu turpis egestas pretium aenean pharetra magna ac. Scelerisque felis imperdiet proin fermentum. Duis at tellus at urna condimentum mattis. Nunc sed augue lacus viverra vitae congue eu consequat. Donec pretium vulputate sapien nec sagittis aliquam malesuada. Magna fermentum iaculis eu non diam phasellus. Condimentum mattis pellentesque id nibh tortor.</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse
+          platea dictumst vestibulum rhoncus est. In massa tempor nec feugiat
+          nisl pretium. Adipiscing enim eu turpis egestas pretium aenean
+          pharetra magna ac. Scelerisque felis imperdiet proin fermentum. Duis
+          at tellus at urna condimentum mattis. Nunc sed augue lacus viverra
+          vitae congue eu consequat. Donec pretium vulputate sapien nec sagittis
+          aliquam malesuada. Magna fermentum iaculis eu non diam phasellus.
+          Condimentum mattis pellentesque id nibh tortor.
+        </p>
       </section>
+      <MapChart />
     </div>
   );
 }
