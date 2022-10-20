eae3959788abce38ee19b27a14e6ba641801f6c1
Ivo Desktop <ivo.k.eilertsen@hiof.no>
2022-10-20 18:09:58 +0200

    Masse css

--- src/pages/Article4.js
+++ src/pages/Article4.js
@@ -4,36 +4,88 @@ import Timeline from "../components/Timeline";
 export default function Article4() {
   return (
     <div className={"article"}>
-      <h1 className={"articleTitle"}>Title 4</h1>
-      <span className={"articleUnderTitle"}>Lorem ipsum dolor sit amet</span>
-      <section className={"articleIntro"}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua
-      </section>
-      <section className={"articleContent"}>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse
-          platea dictumst vestibulum rhoncus est. In massa tempor nec feugiat
-          nisl pretium. Adipiscing enim eu turpis egestas pretium aenean
-          pharetra magna ac. Scelerisque felis imperdiet proin fermentum. Duis
-          at tellus at urna condimentum mattis. Nunc sed augue lacus viverra
-          vitae congue eu consequat. Donec pretium vulputate sapien nec sagittis
-          aliquam malesuada. Magna fermentum iaculis eu non diam phasellus.
-          Condimentum mattis pellentesque id nibh tortor.
-        </p>
-        <br></br>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse
-          platea dictumst vestibulum rhoncus est. In massa tempor nec feugiat
-          nisl pretium. Adipiscing enim eu turpis egestas pretium aenean
-          pharetra magna ac. Scelerisque felis imperdiet proin fermentum. Duis
-          at tellus at urna condimentum mattis. Nunc sed augue lacus viverra
-          vitae congue eu consequat. Donec pretium vulputate sapien nec sagittis
-          aliquam malesuada. Magna fermentum iaculis eu non diam phasellus.
-          Condimentum mattis pellentesque id nibh tortor.
-        </p>
+      <section className={"testPerson"}></section>
+      <section className={"article4Area"}>
+        <section className={"articleContent"}>
+          <h1 className={"article4Title"}>Tengs-drapet</h1>
+          <span className={"articleUnderTitle"}>Lorem ipsum dolor sit amet</span>
+          <section className={"articleIntro"}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua
+          </section>
+          <br></br>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse
+            platea dictumst vestibulum rhoncus est. In massa tempor nec feugiat
+            nisl pretium. Adipiscing enim eu turpis egestas pretium aenean
+            pharetra magna ac. Scelerisque felis imperdiet proin fermentum. Duis
+            at tellus at urna condimentum mattis. Nunc sed augue lacus viverra
+            vitae congue eu consequat. Donec pretium vulputate sapien nec sagittis
+            aliquam malesuada. Magna fermentum iaculis eu non diam phasellus.
+            Condimentum mattis pellentesque id nibh tortor.
+          </p>
+          <br></br>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse
+            platea dictumst vestibulum rhoncus est. In massa tempor nec feugiat
+            nisl pretium. Adipiscing enim eu turpis egestas pretium aenean
+            pharetra magna ac. Scelerisque felis imperdiet proin fermentum. Duis
+            at tellus at urna condimentum mattis. Nunc sed augue lacus viverra
+            vitae congue eu consequat. Donec pretium vulputate sapien nec sagittis
+            aliquam malesuada. Magna fermentum iaculis eu non diam phasellus.
+            Condimentum mattis pellentesque id nibh tortor.
+          </p>
+          <br></br>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse
+            platea dictumst vestibulum rhoncus est. In massa tempor nec feugiat
+            nisl pretium. Adipiscing enim eu turpis egestas pretium aenean
+            pharetra magna ac. Scelerisque felis imperdiet proin fermentum. Duis
+            at tellus at urna condimentum mattis. Nunc sed augue lacus viverra
+            vitae congue eu consequat. Donec pretium vulputate sapien nec sagittis
+            aliquam malesuada. Magna fermentum iaculis eu non diam phasellus.
+            Condimentum mattis pellentesque id nibh tortor.
+          </p>
+          <br></br>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse
+            platea dictumst vestibulum rhoncus est. In massa tempor nec feugiat
+            nisl pretium. Adipiscing enim eu turpis egestas pretium aenean
+            pharetra magna ac. Scelerisque felis imperdiet proin fermentum. Duis
+            at tellus at urna condimentum mattis. Nunc sed augue lacus viverra
+            vitae congue eu consequat. Donec pretium vulputate sapien nec sagittis
+            aliquam malesuada. Magna fermentum iaculis eu non diam phasellus.
+            Condimentum mattis pellentesque id nibh tortor.
+          </p>
+          <br></br>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse
+            platea dictumst vestibulum rhoncus est. In massa tempor nec feugiat
+            nisl pretium. Adipiscing enim eu turpis egestas pretium aenean
+            pharetra magna ac. Scelerisque felis imperdiet proin fermentum. Duis
+            at tellus at urna condimentum mattis. Nunc sed augue lacus viverra
+            vitae congue eu consequat. Donec pretium vulputate sapien nec sagittis
+            aliquam malesuada. Magna fermentum iaculis eu non diam phasellus.
+            Condimentum mattis pellentesque id nibh tortor.
+          </p>
+          <br></br>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse
+            platea dictumst vestibulum rhoncus est. In massa tempor nec feugiat
+            nisl pretium. Adipiscing enim eu turpis egestas pretium aenean
+            pharetra magna ac. Scelerisque felis imperdiet proin fermentum. Duis
+            at tellus at urna condimentum mattis. Nunc sed augue lacus viverra
+            vitae congue eu consequat. Donec pretium vulputate sapien nec sagittis
+            aliquam malesuada. Magna fermentum iaculis eu non diam phasellus.
+            Condimentum mattis pellentesque id nibh tortor.
+          </p>
+        </section>
       </section>
       <Timeline  />
     </div>
