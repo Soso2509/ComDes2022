d56a393121abc5b2038049ccd47d95c1311c6890
Ivo <ivo.k.eilertsen@hiof.no>
2022-10-21 13:39:20 +0200

    tester meg frem med ikoner

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaArrowAltCircleUp } from 'react-icons/fa';
+
 
 export default function Footer() {
   /*Hentet fra https://stackabuse.com/how-to-scroll-to-top-in-react-with-a-button-component/ */
@@ -10,8 +12,8 @@ export default function Footer() {
   };
   return (
     <footer>
-      <button onClick={goToTop} className={"upButton"}>Til Toppen</button>
-      <h3>Newsroom 3 Team 4, Ivo Eilertsen, Joachim Fjell, Sofie Karlsen</h3>
+      <button onClick={goToTop} className={"upButton"}><FaArrowAltCircleUp /></button>
+      <h3>Newsroom 3 Team 4, Ivo Eilertsen, Joachim Fjell, Sofie Karlsen</h3><button onClick={goToTop} className={"upButton"}><FaArrowAltCircleUp /></button>
     </footer>
   );
 }
