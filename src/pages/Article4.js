7746409d745ed541f688bf6ab2cd88109b4f2d0d
Sofie Helene Karlsen <sofiehk@gmail.com>
2022-10-25 12:50:30 +0200

    Lagt inn tidslinje for temp

--- src/pages/Article4.js
+++ src/pages/Article4.js
@@ -1,41 +1,41 @@
 import React from "react";
-import Timeline from "../components/Timeline";
+import { Timeline } from "../components/Timeline";
 
 export default function Article4() {
   return (
     <div className={"article"}>
-        <section className={"articleContent"}>
-          <h1 className={"articleTitle"}>Artikkel 4</h1>
-          <span className={"articleUnderTitle"}>Lorem ipsum dolor sit amet</span>
-          <section className={"articleIntro"}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua
-          </section>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse
-            platea dictumst vestibulum rhoncus est. In massa tempor nec feugiat
-            nisl pretium. Adipiscing enim eu turpis egestas pretium aenean
-            pharetra magna ac. Scelerisque felis imperdiet proin fermentum. Duis
-            at tellus at urna condimentum mattis. Nunc sed augue lacus viverra
-            vitae congue eu consequat. Donec pretium vulputate sapien nec sagittis
-            aliquam malesuada. Magna fermentum iaculis eu non diam phasellus.
-            Condimentum mattis pellentesque id nibh tortor.
-          </p>
-          <br></br>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse
-            platea dictumst vestibulum rhoncus est. In massa tempor nec feugiat
-            nisl pretium. Adipiscing enim eu turpis egestas pretium aenean
-            pharetra magna ac. Scelerisque felis imperdiet proin fermentum. Duis
-            at tellus at urna condimentum mattis. Nunc sed augue lacus viverra
-            vitae congue eu consequat. Donec pretium vulputate sapien nec sagittis
-            aliquam malesuada. Magna fermentum iaculis eu non diam phasellus.
-            Condimentum mattis pellentesque id nibh tortor.
-          </p>
+      <section className={"articleContent"}>
+        <h1 className={"articleTitle"}>Artikkel 4</h1>
+        <span className={"articleUnderTitle"}>Lorem ipsum dolor sit amet</span>
+        <section className={"articleIntro"}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua
         </section>
-      <Timeline  />
+        <Timeline />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse
+          platea dictumst vestibulum rhoncus est. In massa tempor nec feugiat
+          nisl pretium. Adipiscing enim eu turpis egestas pretium aenean
+          pharetra magna ac. Scelerisque felis imperdiet proin fermentum. Duis
+          at tellus at urna condimentum mattis. Nunc sed augue lacus viverra
+          vitae congue eu consequat. Donec pretium vulputate sapien nec sagittis
+          aliquam malesuada. Magna fermentum iaculis eu non diam phasellus.
+          Condimentum mattis pellentesque id nibh tortor.
+        </p>
+        <br></br>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Habitasse
+          platea dictumst vestibulum rhoncus est. In massa tempor nec feugiat
+          nisl pretium. Adipiscing enim eu turpis egestas pretium aenean
+          pharetra magna ac. Scelerisque felis imperdiet proin fermentum. Duis
+          at tellus at urna condimentum mattis. Nunc sed augue lacus viverra
+          vitae congue eu consequat. Donec pretium vulputate sapien nec sagittis
+          aliquam malesuada. Magna fermentum iaculis eu non diam phasellus.
+          Condimentum mattis pellentesque id nibh tortor.
+        </p>
+      </section>
     </div>
   );
 }
