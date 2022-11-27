61430a9450e4e12371682993fd241b54ea99c6bd
Sofie K <sofiehk@gmail.com>
2022-11-27 12:00:24 +0100

    Fikset modaler, men nå kan bare lukkes med close knapp

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,32 +1,12 @@
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 
 // Kode hentet fra https://github.com/trananhtuat/react-modal
 
 const Modal = (props) => {
   const modalRef = useRef();
-
-  useEffect(() => {
-    const clickOutsideContent = (e) => {
-      if (e.target === modalRef.current) {
-        props.setShow(false);
-      }
-    };
-    window.addEventListener("click", clickOutsideContent);
-    return () => {
-      window.removeEventListener("click", clickOutsideContent);
-    };
-  }, [props]);
-
   return (
     <div ref={modalRef} className={`modal ${props.show ? "active" : ""}`}>
-      <div className="modal__content">
-        {!props.hideCloseButton && (
-          <span onClick={() => props.setShow(false)} className="modal__close">
-            &times;
-          </span>
-        )}
-        {props.children}
-      </div>
+      <div className="modal__content">{props.children}</div>
     </div>
   );
 };
